220a67759d7952162a009a2f57cd21f06d0e53af
SehyeonOh <sehyeon@dnotitia.com>
2024-12-20 07:46:09 +0000

    modified one stop script run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,21 @@
-(cd src/engine; docker build -t osseahorse .)
-(cd src/api; docker build -t oscoral .)
-docker compose up
+#!/bin/sh
 
-curl -X POST http://localhost:3000/v0/nodes        -H "Content-Type: application/json"        -d '{"nodes": [{"name": "node1", "endpoint": "redis://127.0.0.1:6388"}, {"name":"node2", "endpoint":"redis://127.0.0.1:6389"}, {"name":"node3", "endpoint":"redis://127.0.0.1:6390"}] }'
+# Check and build the osseahorse image
+if [ "$(docker images -q osseahorse:latest 2> /dev/null)" = "" ]; then
+  echo "Building osseahorse Docker image..."
+  (cd src/engine && docker build -t osseahorse .)
+else
+  echo "osseahorse Docker image already exists. Skipping build."
+fi
+
+# Check and build the oscoral image
+if [ "$(docker images -q oscoral:latest 2> /dev/null)" = "" ]; then
+  echo "Building oscoral Docker image..."
+  (cd src/api && docker build -t oscoral .)
+else
+  echo "oscoral Docker image already exists. Skipping build."
+fi
+
+# Start the containers using docker-compose
+echo "Starting Docker Compose..."
+docker compose up
